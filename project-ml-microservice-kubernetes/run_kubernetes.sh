c048a388090e58b8bf3ce836683da4600ef229ad
thanhntphys <thanhntphys@gmail.com>
2022-12-31 00:03:34 +0700

    update kebernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=thanhntphys/udacity-cdo-prj4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-cdo-prj4-latest \
+    --image=$dockerpath \
+    --port=80 \
+    --labels app=udacity-cdo-prj4-latest
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-cdo-prj4-latest 8000:80
