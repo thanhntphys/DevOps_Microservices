23351f66b1a6ab9e0f003f3ac0b19ee6eb523b09
thanhntphys <thanhntphys@gmail.com>
2022-12-30 23:44:55 +0700

    update upload docker

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=thanhnt97/udacity-cdo-prj4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag microservice_image:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
