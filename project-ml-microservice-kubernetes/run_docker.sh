c6434c27146179e12afeb39cbcba3d3d4fcba225
thanhntphys <thanhntphys@gmail.com>
2022-12-30 22:44:26 +0700

    task2

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker.exe build --tag phuongtd-app:latest .
 
 # Step 2: 
 # List docker images
+docker.exe images
 
 # Step 3: 
 # Run flask app
+docker.exe run -it -p 8000:80 phuongtd-app:latest
